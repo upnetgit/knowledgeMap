19e2ffb1c19211e25a32198039eff1b4097662bb
Haoyang Qin <allenalla757@gmail.com>
2026-05-06 13:06:15 +0800

    Update __init__.py

--- xmodaler/evaluation/__init__.py
+++ xmodaler/evaluation/__init__.py
@@ -10,3 +10,23 @@ from .coco_printer import COCOPrinter
 from .vqa_evaler import VQAEvaler
 from .vcr_evaler import VCREvaler
 from .retrieval_evaler import RetrievalEvaler
+
+try:
+	from .relation_eval import compute_metrics, load_jsonl, save_report
+except Exception:  # pragma: no cover - optional helper should not break base evaluation imports
+	compute_metrics = None
+	load_jsonl = None
+	save_report = None
+
+__all__ = [
+	"build_evaluation",
+	"COCOEvaler",
+	"COCOPrinter",
+	"VQAEvaler",
+	"VCREvaler",
+	"RetrievalEvaler",
+	"compute_metrics",
+	"load_jsonl",
+	"save_report",
+]
+
